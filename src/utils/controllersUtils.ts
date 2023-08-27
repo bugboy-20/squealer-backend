82200b23a004a36b17f00ff82f54aec4c07c56ed
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-08-27 19:10:50 +0200

    Ups forgot this

---
+++ src/utils/controllersUtils.ts
@@ -0,0 +1,24 @@
+import {RequestHandler} from "express";
+
+const catchServerError = function (f: RequestHandler, errorCode = 500, errorMessage?: string, responce?: (() => void) | undefined) : RequestHandler {
+  let r : RequestHandler = async (req, res, next) => {
+    try {
+      f(req,res,next)
+    } catch(e) {
+      if (errorMessage)
+        console.error(errorMessage)
+      else
+        console.error('Error ' + errorMessage + ':')
+
+      console.error(e)
+      res.statusCode = errorCode
+      res.end(responce)
+      res
+    }
+  }
+
+  return r
+}
+
+
+export { catchServerError }
