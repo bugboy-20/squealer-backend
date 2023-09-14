cc7c4b096a28e49733a8193953a2f342ccae17ad
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2023-09-14 16:13:00 +0200

    FIX slom bug

--- src/middleware/esapeChars.ts
+++ src/middleware/esapeChars.ts
@@ -2,11 +2,29 @@ import {RequestHandler} from "express"
 
 const escapeParam = (param: string) => {
     const fn : RequestHandler = (req,res,next) => {
-        req.params[param] = req.params[param].replace(/%C2%A7/i, '§') // '§' issue
+        if(req.params[param])
+          req.params[param] = req.params[param].replace(/%C2%A7/i, '§') // '§' issue
         next()
     }
 
     return fn
 }
 
-export { escapeParam }
+const escapeQuery = (param: string) => {
+    const fn : RequestHandler = (req,res,next) => {
+        if (req.query[param]) {
+          let newQuery = req.query[param];
+
+          if (typeof newQuery === 'string')
+            newQuery = newQuery.replace(/%C2%A7/i, '§'); // '§' issue
+
+          req.query[param] = newQuery;
+        }
+        next()
+    }
+
+    return fn
+}
+
+
+export { escapeParam, escapeQuery }
