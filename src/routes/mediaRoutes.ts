181ce1fdba80ff759bef63720b43800bd643a621
jantizio <emanuele0246@gmail.com>
2023-09-18 22:04:30 +0200

    filtro dei file

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -4,11 +4,19 @@ import multer from "multer";
 import {uploadMedia, getMedia} from "../controllers/mediaController";
 const upload = multer({
   storage: multer.memoryStorage(),
-  /*limits: {
-    fields: 0, // Max number of non-file fields
+  fileFilter(req, file, callback) {
+      if(file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+          callback(null, true)
+      }
+      else {
+          callback(new Error('Only images and videos are allowed'))
+      }
+  },
+  limits: {
     files: 1, // For multipart forms, the max number of file fields
-    parts: 1, // For multipart forms, the max number of parts (fields + files)
-  },*/
+    // fields: 0, // Max number of non-file fields
+    // parts: 1, // For multipart forms, the max number of parts (fields + files)
+  },
 });
 
 const mediaRoutes : (app : polka.Polka) => void = app => {
