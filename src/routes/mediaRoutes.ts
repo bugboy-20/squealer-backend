ff95626e84d8ec4aee7203f7d762c39bdf88bf70
jantizio <emanuele0246@gmail.com>
2023-09-21 18:58:06 +0200

    multer error handled correctly

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -1,28 +1,32 @@
-
-import polka from "polka"
-import multer from "multer";
-import {uploadMedia, getMedia} from "../controllers/mediaController";
+import polka from 'polka';
+import multer from 'multer';
+import { uploadMedia, getMedia } from '../controllers/mediaController';
+import { catchServerError } from '../utils/controllersUtils';
 const upload = multer({
   storage: multer.memoryStorage(),
   fileFilter(req, file, callback) {
-      if(file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
-          callback(null, true)
-      }
-      else {
-          callback(new Error('Only images and videos are allowed'))
-      }
+    if (
+      file.mimetype.startsWith('image/') ||
+      file.mimetype.startsWith('video/')
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Sono permessi solo file immagine e video'));
+    }
   },
   limits: {
     files: 1, // For multipart forms, the max number of file fields
-    // fields: 0, // Max number of non-file fields
-    // parts: 1, // For multipart forms, the max number of parts (fields + files)
   },
 });
 
-const mediaRoutes : (app : polka.Polka) => void = app => {
-    app
-      .post('/api/media/', upload.single('media'), uploadMedia)
-      .get('/api/media/:id?', getMedia)
-}
+const mediaRoutes: (app: polka.Polka) => void = (app) => {
+  app
+    .post(
+      '/api/media/',
+      catchServerError(upload.single('media'), 406),
+      uploadMedia
+    )
+    .get('/api/media/:id', getMedia);
+};
 
 export default mediaRoutes;
