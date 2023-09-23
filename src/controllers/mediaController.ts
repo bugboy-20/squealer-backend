27ba198803024ae175856d458ac98fe74e9e0181
jantizio <emanuele0246@gmail.com>
2023-09-23 14:03:04 +0200

    working video compression very inefficient

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -5,6 +5,8 @@ import { bucket } from '../db_utils';
 import sharp from 'sharp';
 import ffmpeg from 'fluent-ffmpeg';
 import stream from 'stream';
+import tmp from 'tmp-promise';
+import fs from 'fs';
 
 export const compressMedia: RequestHandler = catchServerError(
   async (req, res, next) => {
@@ -35,38 +37,47 @@ export const compressMedia: RequestHandler = catchServerError(
         compressedBuffer = await sharp(file.buffer)
           .toFormat(metadata.format, { quality: 70, mozjpeg: true }) // Use the input format for output
           .toBuffer();
-      } else {
-        // if the file is not an image, it must be a video
-        const inputStream = stream.Readable.from(file.buffer, {
-          objectMode: false,
-        });
 
-        // Create a writable stream to collect the optimized video
-        const outputStream = new stream.PassThrough();
+        if (compressedBuffer) file.buffer = compressedBuffer;
+        console.log(
+          `After compressing ${Math.floor(
+            Buffer.byteLength(file.buffer) / 1024
+          )} KB`
+        );
+        next();
+      } else {
+        const { path, cleanup } = await tmp.dir({
+          unsafeCleanup: true,
+        });
+        const inputFilePath = `${path}/${file.originalname}`;
+        fs.writeFileSync(inputFilePath, file.buffer);
+
+        const outputfilePath = `${path}/tmp_${file.originalname}`;
 
         // Create a FFmpeg command to optimize the video
-        const command = ffmpeg(inputStream)
+        ffmpeg(inputFilePath)
+          .output(outputfilePath)
           .videoCodec('libx264') // Set video codec for compression
           .audioCodec('aac') // Set audio codec
           .on('start', (commandLine) => {
             console.log('FFmpeg command:', commandLine);
           })
           .on('end', () => {
-            outputStream.end();
-          });
-
-        // Collect the optimized video in a buffer
-        const chunks: Uint8Array[] = [];
-        outputStream.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
-
-        outputStream.on('end', () => {
-          compressedBuffer = Buffer.concat(chunks);
-        });
-
-        // Pipe the output of FFmpeg to the writable stream
-        command.pipe(outputStream, { end: true });
+            file.buffer = fs.readFileSync(outputfilePath);
+            cleanup();
+            console.log(
+              `After compressing ${Math.floor(
+                Buffer.byteLength(file.buffer) / 1024
+              )} KB`
+            );
+            next();
+          })
+          .on('error', (err) => {
+            console.error('Error optimizing mammt ', err);
+            cleanup();
+            next();
+          })
+          .run();
       }
     } catch (error) {
       // if something goes wrong, just continue with the original file
@@ -74,14 +85,6 @@ export const compressMedia: RequestHandler = catchServerError(
       console.log('Error compressing media file');
       next();
     }
-
-    if (compressedBuffer) file.buffer = compressedBuffer;
-    console.log(
-      `After compressing ${Math.floor(
-        Buffer.byteLength(file.buffer) / 1024
-      )} KB`
-    );
-    next();
   },
   500,
   'Error compressing media file',
@@ -117,7 +120,7 @@ const uploadMedia: RequestHandler = catchServerError(async (req, res) => {
   uploadStream.on('finish', () => {
     const mediaUrl = `${req.headers['x-forwarded-proto'] ?? 'http'}://${
       req.headers.host
-    }${req.url}${uploadStream.id}`;
+    }/api/media/${uploadStream.id}`;
 
     res.writeHead(200, {
       'Content-Type': 'text/plain',
