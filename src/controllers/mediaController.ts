fce18bc537216184c54ea18b12b62928a78895b8
jantizio <emanuele0246@gmail.com>
2023-09-23 10:06:34 +0200

    vide optimization not working

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { catchServerError } from '../utils/controllersUtils';
 import { bucket } from '../db_utils';
 import sharp from 'sharp';
+import ffmpeg from 'fluent-ffmpeg';
+import stream from 'stream';
 
 export const compressMedia: RequestHandler = catchServerError(
   async (req, res, next) => {
@@ -33,6 +35,38 @@ export const compressMedia: RequestHandler = catchServerError(
         compressedBuffer = await sharp(file.buffer)
           .toFormat(metadata.format, { quality: 70, mozjpeg: true }) // Use the input format for output
           .toBuffer();
+      } else {
+        // if the file is not an image, it must be a video
+        const inputStream = stream.Readable.from(file.buffer, {
+          objectMode: false,
+        });
+
+        // Create a writable stream to collect the optimized video
+        const outputStream = new stream.PassThrough();
+
+        // Create a FFmpeg command to optimize the video
+        const command = ffmpeg(inputStream)
+          .videoCodec('libx264') // Set video codec for compression
+          .audioCodec('aac') // Set audio codec
+          .on('start', (commandLine) => {
+            console.log('FFmpeg command:', commandLine);
+          })
+          .on('end', () => {
+            outputStream.end();
+          });
+
+        // Collect the optimized video in a buffer
+        const chunks: Uint8Array[] = [];
+        outputStream.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+
+        outputStream.on('end', () => {
+          compressedBuffer = Buffer.concat(chunks);
+        });
+
+        // Pipe the output of FFmpeg to the writable stream
+        command.pipe(outputStream, { end: true });
       }
     } catch (error) {
       // if something goes wrong, just continue with the original file
