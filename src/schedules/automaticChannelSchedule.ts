38dc010acdab5b9356ab33963db4f3b3c2c1d0bb
jantizio <emanuele0246@gmail.com>
2024-01-20 12:25:56 +0100

    fix: bugfixes in trending channel

--- src/schedules/automaticChannelSchedule.ts
+++ src/schedules/automaticChannelSchedule.ts
@@ -23,9 +23,9 @@ const addToControversial: Schedule = [
     }).exec();
 
     for (const squeal of everySqueal) {
-      if (await isSquealControversial(squeal.id)) {
+      if (await isSquealControversial(squeal)) {
         squeal.receivers.push('§CONTROVERSIAL');
-        squeal.save();
+        squeal.save({ disableMiddleware: true });
       }
     }
   },
@@ -38,25 +38,32 @@ const addToTrending: Schedule = [
       receivers: '§TRENDING',
     }).exec();
 
-    // get 20 squeals with the most positive reactions
-    const newTrendingSqueals = await SquealModel.find({})
-      .sort({ positive_reactions: -1 })
-      .limit(20)
-      .exec();
-
     // remove the old trending squeals
     for (const squeal of trendingSqueals) {
       const index = squeal.receivers.indexOf('§TRENDING');
       if (index > -1) {
         squeal.receivers.splice(index, 1);
       }
-      squeal.save();
+      squeal.save({ disableMiddleware: true });
     }
+
+    // get 20 squeals with the most positive reactions
+    const newTrendingSqueals = await SquealModel.aggregate([
+      {
+        $addFields: {
+          positive_reaction_count: { $size: '$positive_reaction' },
+        },
+      },
+      { $sort: { positive_reaction_count: -1 } },
+      { $limit: 20 },
+    ]);
 
     // add the new trending squeals
     for (const squeal of newTrendingSqueals) {
-      squeal.receivers.push('§TRENDING');
-      squeal.save();
+      await SquealModel.updateOne(
+        { _id: squeal._id },
+        { $push: { receivers: '§TRENDING' } }
+      );
     }
   },
 ];
