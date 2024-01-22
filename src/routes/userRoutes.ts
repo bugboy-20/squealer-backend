476c783f1419139c3037a9dd7cee0b4c667780e1
jantizio <emanuele0246@gmail.com>
2024-01-22 11:52:09 +0100

    feat: buy quote midlleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import { Express } from "express"
 
-import {listAllUsers, addUser, deleteUser, findUser, getQuote, whoiam, addSMM, deleteSMM, changePassword, resetPassword, changeQuote, changeBlockedStatus, } from "../controllers/userController";
+import {listAllUsers, addUser, deleteUser, findUser, getQuote, whoiam, addSMM, deleteSMM, changePassword, resetPassword, changeQuote, changeBlockedStatus, buyQuote, } from "../controllers/userController";
 import { getNotifications } from "../controllers/squealController";
 import {and, auth, isModerator, sameUsername, isAuth, or} from "../middleware/auth";
 import {parseJWT} from "../middleware/parseJWT";
@@ -15,6 +15,7 @@ const userRoutes : (app : Express) => Express = app =>
       .get('/api/users/me', auth(isAuth, whoiam), send401)
       .get('/api/users/:username', findUser)// auth(isAuth, findUser), send401)
       .get('/api/users/me/notifications', auth(isAuth, getNotifications), send401)
+      .post('/api/users/me/quota/buy', buyQuote)
       .get('/api/users/:username/subscriptions', send501) //TODO
       .patch('/api/users/:username/password', auth(sameUsername, changePassword), send401)
       .get('/api/users/:username/quota', auth(or(isModerator,sameUsername), getQuote), send401)
